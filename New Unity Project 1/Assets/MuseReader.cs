66b903928b276d1088a98ef45c6c9bae2a3443ec
ceaston7 <ceaston7@hotmail.com>
2016-10-18 18:46:56 -0400

    LookingAt and MindMove
    MindMove does not actually implement Muse data.

---
+++ New Unity Project 1/Assets/MuseReader.cs
@@ -0,0 +1,29 @@
+﻿/*using UnityEngine;
+using System.Collections;
+using SharpOSC;
+
+public class MuseReader : MonoBehaviour {
+    public static int alpha_val;
+	// Use this for initialization
+	void Start () {
+        HandleOscPacket callback = delegate (OscPacket packet) {
+            var messageReceived = (OscMessage)packet;
+            var addr = messageReceived.Address;
+        };
+        var listener = new TCPListener(5000, callback);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //bring OSC data to Unity
+        if (OscPacketIO.isOpen())
+        {
+            alpha_val = GetNumericValue("/mus/elements/alpha_relative", Int32);
+            if (alpha_val > 0.2)
+            {
+                //raise box
+            }
+        }
+    }
+}
+*/
---
+++ New Unity Project 1/Assets/lookingAt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookingAt : MonoBehaviour
+{
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("script working");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(transform.position, transform.forward, out hit);
+        if (hit.collider != null && hit.collider.CompareTag("lift"))
+        {
+            Debug.Log("Looking at " + hit.collider.name);
+            //hit.collider.GetComponent<mindMove>().move();
+        }
+    }
+}
